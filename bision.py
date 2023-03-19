210722cba1da22b51aaeabaff497f842f7bddbc5
Adhikari-Ashutosh <ashutosh_adhikari@yahoo.com>
2023-03-20 00:03:48 +0530

    first commit

---
+++ bision.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from google.cloud import vision
+print("Test1")
+app = Flask(__name__)
+client = vision.ImageAnnotatorClient()
+@app.route("/" , methods=['POST'])
+def testAPI():
+    image_uri = request.json.get('image_uri')
+    response = client.annotate_image({
+  'image': {'source': {'image_uri': image_uri}},
+  'features': [{'type_': vision.Feature.Type.LABEL_DETECTION}]
+        })
+    print(response)
+    return response
